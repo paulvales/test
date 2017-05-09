1128a95f60472e29dc3abae73335c92ebff68c9c
paulvales <site@webpaul.ru>
2017-05-09 21:28:45 +0300

    perviy commit

---
+++ backup/EVtYGI8i.job.php
@@ -0,0 +1,28 @@
+<?php
+$JOB = array (
+  'act' => 'restore',
+  'type' => 'run',
+  'db' => 'text',
+  'charset' => '0',
+  'file' => 'dump.sql',
+  'strategy' => '0',
+  'correct' => '0',
+  'autoinc' => '0',
+  'prefix_from' => '',
+  'prefix_to' => '',
+  'savesql' => '0',
+  'job' => 'EVtYGI8i',
+  'obj' => NULL,
+  'file_ext' => 'sql',
+  'file_name' => 'backup/dump.sql',
+  'file_tmp' => 'backup/dump.sql',
+  'file_rtl' => 'backup/EVtYGI8i.rtl',
+  'file_log' => 'backup/EVtYGI8i.log',
+  'file_stp' => 'backup/EVtYGI8i.stp',
+  'eol' => '
+',
+  'todo' => 
+  array (
+  ),
+);
+?>
---
+++ ses.php
@@ -0,0 +1,33 @@
+<?php
+$SES = array (
+  '0cce4b945598aed5f3a78fee349622ee' => 
+  array (
+    'cfg' => 
+    array (
+      'charsets' => 'cp1251 utf8 latin1',
+      'lang' => 'ru',
+      'time_web' => '600',
+      'time_cron' => '600',
+      'backup_path' => 'backup/',
+      'backup_url' => 'backup/',
+      'only_create' => 'MRG_MyISAM MERGE HEAP MEMORY',
+      'globstat' => 0,
+      'my_host' => 'localhost',
+      'my_port' => 3306,
+      'my_user' => 'root',
+      'my_pass' => '',
+      'my_comp' => 0,
+      'my_db' => '',
+      'auth' => 'mysql cfg',
+      'user' => '',
+      'pass' => '',
+      'confirm' => '30',
+      'exitURL' => './',
+      'outfile_path' => 'backup/',
+      'outfile_size' => '64',
+    ),
+    'time' => 1478966896,
+    'lng' => 'ru',
+  ),
+);
+?>
